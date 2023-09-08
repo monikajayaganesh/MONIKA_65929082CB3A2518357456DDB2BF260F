46d49c400b2454a1540342d5f5f20baa97fbc0e0
monikajayaganesh <144433373+monikajayaganesh@users.noreply.github.com>
2023-09-08 15:01:09 +0530

    Add files via upload

---
+++ monika/main (1).py
@@ -0,0 +1,51 @@
+#implement a recursive function to calculate the factorial of a given number
+1!=1*1
+2!=2*1!___>2*1
+3!=3*2!___>3*2*1
+
+
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n_1)
+    number 6
+      res=fact_rec(number)
+    printf("the factorial of {}is{}".format(number,res))
+  
+    
+           
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+==1
+    
---
+++ monika/main (3).py
@@ -0,0 +1,11 @@
+#leap year
+def isleapyear(year):
+  if ((year%4==0)or(year%100!=0)and(year% 4==0)):
+    return True
+  else:
+    return False
+year=int(input("enter a year :"))
+if isleapyear(year):
+  print("{} is a leapyear.".format(year))
+else:
+    print("{} is not a leapyear.".format(year))
